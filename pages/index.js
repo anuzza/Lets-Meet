8f08a261b9f6e2280c612ad1eed50133abc12ea8
anuzza <anuja3580@gmail.com>
2022-07-17 03:51:13 -0500

    outputting a list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,33 @@
 //our-domain.com/
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some adress 5, 12345 Some city",
+  },
+
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some adress 6, 12945 Some city",
+  },
+
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some adress 2, 12465 Some city",
+  },
+];
+
+function Homepage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+export default Homepage;
