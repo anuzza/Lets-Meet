faecfc917c1fadedeae2c0ff7e260e8a2ff8b368
anuzza <anuja3580@gmail.com>
2022-07-18 06:49:14 -0500

    added detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,8 @@
+//our-domain.com/meetupId
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function meetupDetails() {
+  return <MeetupDetail />;
+}
+export default meetupDetails;
